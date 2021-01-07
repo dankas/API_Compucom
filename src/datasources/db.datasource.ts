cf098ea70b96aa657875336b5ec2a552f25d53d6
Daniel Moraes <dankasmoraes@gmail.com>
2021-01-07 16:28:47 -0300

    feat: :tada: Configuração inicial do projeto
    Foi configurado o framework (loopback4) e suas dependencias.

---
+++ src/datasources/db.datasource.ts
@@ -0,0 +1,35 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+require('dotenv').config();
+const usrdb = process.env.DB_USR;
+const pswdb = process.env.DB_PSW;
+const hostdb = process.env.DB_HOST;
+
+const config = {
+  name: 'db',
+  connector: 'mysql',
+  url: '',
+  host: hostdb,
+  port: 3306,
+  user: usrdb,
+  password: pswdb,
+  database: 'api_cpc'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
